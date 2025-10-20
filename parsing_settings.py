644a0a2c940c66a212a2e2c1671fd8fbba98e9ec
Oleksandr Hrynenko <grinenkoa@gmail.com>
2025-10-20 13:56:29 +0300

    change parsing_settings from json to Excel, update requirements.txt, delete parsing_settings.json

--- parsing_settings.py
+++ parsing_settings.py
@@ -1,71 +1,29 @@
 # parsint_settings.py
-# This script is used to load and display parsing settings from a JSON file.
+# This script is used to load and return parsing settings from a Excel file.
 
-import json
+import pandas as pd
 
-# Read parsing settings from a JSON file
-# The JSON file should contain the structure of networks, categories, and cities for parsing
-with open('parsing_setting.json', 'r', encoding='utf-8') as file:
-    parsing_settings = json.load(file)
-
-
-# Function to get parsing networks settings
-def get_nets_parsing_settings() -> list:
-    nets_settings = []
-    for net in parsing_settings:
-        net_info = {
-            'net_name': net['net_name'],
-            'net_main_url': net['url'],
-            'parser': net['parser']
+def get_parsing_settings(file_path = "parsing_settings.xlsx"):
+    """"
+    Generates parsing settings from an Excel file.
+    Each setting is represented as a dictionary.
+    """
+    df = pd.read_excel(file_path, engine="openpyxl")
+    df = df[df['ToParse'] == 1]
+    for index, row in df.iterrows():
+        setting_by_row = {
+            "net_name": row['net_name'],
+            "url": row['url'],
+            "category_name": row['category_name'],
+            "category_url": row['category_url'],
+            "parser": row['parser'],
+            "city_name": row['city_name'],
+            "shop_address": row['shop_address']
         }
-        nets_settings.append(net_info)
-    return nets_settings
-
-# Function to get categories by network name
-def get_categories_by_net_name(net_name: str) -> list:
-    for net in parsing_settings:
-        if net['net_name'] == net_name:
-            return net['categories']
-    return []
+        yield setting_by_row
 
 
-# Function to get cities by network name
-def get_cities_by_net_name(net_name: str) -> list:
-    for net in parsing_settings:
-        if net['net_name'] == net_name:
-            net_cities = []
-            for city in net['cities']:
-                net_cities.append(city['city_name'])
-            return net_cities
-    return [] 
 
-
-# Function to get shops by network name and city name
-def get_shops_by_net_in_city(net_name: str, city_name: str) -> list:
-     for net in parsing_settings:
-        if net['net_name'] == net_name:
-            for city in net['cities']:
-                if city['city_name'] == city_name:
-                    return city['shop-address']
-                return []
-            return []
-        return []
-     return []
-        
-
-
-# for net in parsing_settings:
-    # print(net)
-    # print(f"Net: {net['net_name']}")
-#     print(f"URL: {net['url']}")
-#     for category in net['categories']:
-#         print(f"  Category: {category['category_name']}")
-#         print(f"  Category URL: {category['category_url']}")
-#     print(f"Parser: {net['parser']}")
-#     for city in net['cities']:
-#         print(f"  City: {city['city_name']}")
-#         for shop in city['shop-address']:
-#             print(f"    Shop Address: {shop}")
-    # print('' + '-' * 40)
-
-# 
+if __name__ == "__main__":
+    for setting in get_parsing_settings():
+        print(setting)
