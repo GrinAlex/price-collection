13169e711c7b13661e803f334425e261492a9f3c
O.Hrynenko <o.grynenko@bkk-kyiv.com>
2025-11-05 12:02:07 +0200

    Added save Parsed status by setting row

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 # Import parsing settings
-from parsing_settings import get_parsing_settings
+from parsing_settings import get_parsing_settings, set_parsed_row
 
 # Parsing scripts
 from parsers.varus_parser import parse_varus
@@ -22,6 +22,7 @@ driver = webdriver.Firefox()
 
 # Loop by parsing settings
 for parsing_setting_line in get_parsing_settings():
+    row_number = parsing_setting_line['row_number']
     net_name = parsing_setting_line['net_name']
     net_url = parsing_setting_line['url']
     category_name = parsing_setting_line['category_name']
@@ -38,41 +39,7 @@ for parsing_setting_line in get_parsing_settings():
     
     write_data_to_file(data)
 
-driver.close()
-
-
-
-"""
-
-# Initialize variables
-net = "Varus"
-urls = ["https://varus.ua/tistechtka", "https://varus.ua/torti"]
-curr_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-pause_time = 20  # seconds
-
-# Empty data list to store product cards
-data = []
-
-# Initialize parsing count
-parsintg_count = 0
-
-driver = webdriver.Firefox()
-
-for url in urls:
-    data.extend(parse_varus(driver, url, pause_time))
+    set_parsed_row(row_number)
 
 driver.close()
 
-# Specify the CSV file name
-filename = "data.csv"
-
-# Extract fieldnames from the first dictionary
-fieldnames = data[0].keys()
-
-# Write to CSV
-with open(filename, mode="w", newline="", encoding="utf-8") as file:
-    writer = csv.DictWriter(file, fieldnames=fieldnames)
-    writer.writeheader()  # write header row
-    writer.writerows(data)  # write data rows
-
-"""
--- parsing_settings.py
+++ parsing_settings.py
@@ -9,9 +9,13 @@ def get_parsing_settings(file_path = "parsing_settings.xlsx"):
     Each setting is represented as a dictionary.
     """
     df = pd.read_excel(file_path, engine="openpyxl")
-    df = df[df['ToParse'] == 1]
-    for index, row in df.iterrows():
+    # df_with_index = df.reset_index()
+    df_with_index = df.reset_index().rename(columns={'index': 'row_number'})
+
+    df_with_index = df_with_index[df_with_index['ToParse'] == 1]
+    for index, row in df_with_index.iterrows():
         setting_by_row = {
+            "row_number": row['row_number'],
             "net_name": row['net_name'],
             "url": row['url'],
             "category_name": row['category_name'],
@@ -23,7 +27,15 @@ def get_parsing_settings(file_path = "parsing_settings.xlsx"):
         yield setting_by_row
 
 
+def set_parsed_row(row_number, file_path = "parsing_settings.xlsx"):
+    df = pd.read_excel(file_path)
+    df.loc[row_number, 'Parsed'] = 1
+    df.to_excel(file_path, index=False)
+
+
 
 if __name__ == "__main__":
     for setting in get_parsing_settings():
         print(setting)
+
+    set_parsed_row(2)
